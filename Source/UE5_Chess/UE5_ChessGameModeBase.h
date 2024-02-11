fb164cd8118aa6d785e7417fbad6d842093b48f9
00ste <amalberti75@gmail.com>
2024-02-11 13:23:29 +0100

    First commit

---
+++ Source/UE5_Chess/UE5_ChessGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_ChessGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_CHESS_API AUE5_ChessGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
